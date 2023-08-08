9c9968192a4bdfcea031fbbdf9fccd650ddb2b76
Betoside <betoside@gmail.com>
2023-08-08 18:29:31 -0300

    media query header e side navigation menu finalizados

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ window.addEventListener('scroll', () => {
   const header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+
+// javascript of responsive navigation sidebar menu
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+const navigationItems = document.querySelectorAll('.navigation a');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
+
+navigationItems.forEach((navigationItem) => {
+  navigationItem.addEventListener('click', () => {
+    menuBtn.classList.remove('active');
+    navigation.classList.remove('active');
+  });
+
+});
